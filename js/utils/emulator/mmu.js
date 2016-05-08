c69e16ab0df07a7fa8b22b7c381e10ff72441486
jkoudys <jkoudys@gmail.com>
2016-05-08 11:01:17 -0400

    Move big data chunks into their own space

--- js/utils/emulator/mmu.js
+++ js/utils/emulator/mmu.js
@@ -1,5 +1,10 @@
+/**
+ * The memory management
+ * TODO: This is mostly from the jsemu project and still needs a lot of work
+ * to take full advantage of typed arrays and imports
+ */
 // Flux
-import {log} from '../../actions/LogActions.js';
+import { log } from '../../actions/LogActions.js';
 
 // Game Boy
 import GPU from './gpu.js';
@@ -7,32 +12,17 @@ import Z80 from './z80.js';
 import Keypad from './Keypad.js';
 import Timer from './Timer.js';
 
+import bios from './data/bios';
+
 const MMU = {
-  _bios: new Uint8Array([
-    0x31, 0xFE, 0xFF, 0xAF, 0x21, 0xFF, 0x9F, 0x32, 0xCB, 0x7C, 0x20, 0xFB, 0x21, 0x26, 0xFF, 0x0E,
-    0x11, 0x3E, 0x80, 0x32, 0xE2, 0x0C, 0x3E, 0xF3, 0xE2, 0x32, 0x3E, 0x77, 0x77, 0x3E, 0xFC, 0xE0,
-    0x47, 0x11, 0x04, 0x01, 0x21, 0x10, 0x80, 0x1A, 0xCD, 0x95, 0x00, 0xCD, 0x96, 0x00, 0x13, 0x7B,
-    0xFE, 0x34, 0x20, 0xF3, 0x11, 0xD8, 0x00, 0x06, 0x08, 0x1A, 0x13, 0x22, 0x23, 0x05, 0x20, 0xF9,
-    0x3E, 0x19, 0xEA, 0x10, 0x99, 0x21, 0x2F, 0x99, 0x0E, 0x0C, 0x3D, 0x28, 0x08, 0x32, 0x0D, 0x20,
-    0xF9, 0x2E, 0x0F, 0x18, 0xF3, 0x67, 0x3E, 0x64, 0x57, 0xE0, 0x42, 0x3E, 0x91, 0xE0, 0x40, 0x04,
-    0x1E, 0x02, 0x0E, 0x0C, 0xF0, 0x44, 0xFE, 0x90, 0x20, 0xFA, 0x0D, 0x20, 0xF7, 0x1D, 0x20, 0xF2,
-    0x0E, 0x13, 0x24, 0x7C, 0x1E, 0x83, 0xFE, 0x62, 0x28, 0x06, 0x1E, 0xC1, 0xFE, 0x64, 0x20, 0x06,
-    0x7B, 0xE2, 0x0C, 0x3E, 0x87, 0xF2, 0xF0, 0x42, 0x90, 0xE0, 0x42, 0x15, 0x20, 0xD2, 0x05, 0x20,
-    0x4F, 0x16, 0x20, 0x18, 0xCB, 0x4F, 0x06, 0x04, 0xC5, 0xCB, 0x11, 0x17, 0xC1, 0xCB, 0x11, 0x17,
-    0x05, 0x20, 0xF5, 0x22, 0x23, 0x22, 0x23, 0xC9, 0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B,
-    0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E,
-    0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC,
-    0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E, 0x3c, 0x42, 0xB9, 0xA5, 0xB9, 0xA5, 0x42, 0x4C,
-    0x21, 0x04, 0x01, 0x11, 0xA8, 0x00, 0x1A, 0x13, 0xBE, 0x20, 0xFE, 0x23, 0x7D, 0xFE, 0x34, 0x20,
-    0xF5, 0x06, 0x19, 0x78, 0x86, 0x23, 0x05, 0x20, 0xFB, 0x86, 0x20, 0xFE, 0x3E, 0x01, 0xE0, 0x50
-  ]),
+  _bios: bios,
   _rom: null,
   _carttype: 0,
   _mbc: [{}, {
     rombank: 0,
     rambank: 0,
     ramon: 0,
-    mode: 0
+    mode: 0,
   }],
   _romoffs: 0x4000,
   _ramoffs: 0,
@@ -67,7 +57,7 @@ const MMU = {
       rombank: 0,
       rambank: 0,
       ramon: 0,
-      mode: 0
+      mode: 0,
     };
     MMU._romoffs = 0x4000;
     MMU._ramoffs = 0;
@@ -83,7 +73,7 @@ const MMU = {
     MMU._rom = new Uint8Array(buffer);
     MMU._carttype = MMU._rom[0x0147];
 
-    setTimeout(log, 1, 'mmu', 'ROM loaded, ' + MMU._rom.length + ' bytes');
+    setTimeout(log, 1, 'mmu', `ROM loaded, ${MMU._rom.length} bytes`);
   },
 
   rb(addr) {
@@ -93,7 +83,7 @@ const MMU = {
       case 0x0000:
         if (MMU._inbios) {
           if (addr < 0x0100) return MMU._bios[addr];
-          else if (Z80.getPC() == 0x0100) {
+          else if (Z80.getPC() === 0x0100) {
             MMU._inbios = 0;
             setTimeout(log, 1, 'mmu', 'Leaving BIOS');
           }
@@ -155,47 +145,50 @@ const MMU = {
 
             // Zeropage RAM, I/O, interrupts
           case 0xF00:
-            if (addr == 0xFFFF) {
+            if (addr === 0xFFFF) {
               return MMU._ie;
             } else if (addr > 0xFF7F) {
               return MMU._zram[addr & 0x7F];
-            } else switch (addr & 0xF0) {
-              case 0x00:
-                switch (addr & 0xF) {
-                  case 0:
-                    return Keypad.rb(); // JOYP
-                  case 4:
-                  case 5:
-                  case 6:
-                  case 7:
-                    return Timer.rb(addr);
-                  case 15:
-                    return MMU._if; // Interrupt flags
-                  default:
-                    return 0;
-                }
+            } else {
+              switch (addr & 0xF0) {
+                case 0x00:
+                  switch (addr & 0xF) {
+                    case 0:
+                      return Keypad.rb(); // JOYP
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                      return Timer.rb(addr);
+                    case 15:
+                      return MMU._if; // Interrupt flags
+                    default:
+                      return 0;
+                  }
 
-              case 0x10:
-              case 0x20:
-              case 0x30:
-                return 0;
+                case 0x10:
+                case 0x20:
+                case 0x30:
+                  return 0;
 
-              case 0x40:
-              case 0x50:
-              case 0x60:
-              case 0x70:
-                return GPU.rb(addr);
+                case 0x40:
+                case 0x50:
+                case 0x60:
+                case 0x70:
+                  return GPU.rb(addr);
+              }
             }
         }
+        break;
     }
   },
 
-  rw: function(addr) {
+  rw(addr) {
     // TODO: this could be _way_ simpler if simply using a DataView
     return MMU.rb(addr) + (MMU.rb(addr + 1) << 8);
   },
 
-  wb: function(addr, val) {
+  wb(addr, val) {
     switch (addr & 0xF000) {
       // ROM bank 0
       // MBC1: Turn external RAM on
@@ -203,7 +196,7 @@ const MMU = {
       case 0x1000:
         switch (MMU._carttype) {
           case 1:
-            MMU._mbc[1].ramon = ((val & 0xF) == 0xA) ? 1 : 0;
+            MMU._mbc[1].ramon = ((val & 0xF) === 0xA) ? 1 : 0;
             break;
         }
         break;
@@ -296,49 +289,51 @@ const MMU = {
 
             // Zeropage RAM, I/O, interrupts
           case 0xF00:
-            if (addr == 0xFFFF) {
+            if (addr === 0xFFFF) {
               MMU._ie = val;
             } else if (addr > 0xFF7F) {
               MMU._zram[addr & 0x7F] = val;
-            } else switch (addr & 0xF0) {
-              case 0x00:
-                switch (addr & 0xF) {
-                  case 0:
-                    Keypad.wb(val);
-                    break;
-                  case 4:
-                  case 5:
-                  case 6:
-                  case 7:
-                    Timer.wb(addr, val);
-                    break;
-                  case 15:
-                    MMU._if = val;
-                    break;
-                }
-                break;
+            } else {
+                switch (addr & 0xF0) {
+                case 0x00:
+                  switch (addr & 0xF) {
+                    case 0:
+                      Keypad.wb(val);
+                      break;
+                    case 4:
+                    case 5:
+                    case 6:
+                    case 7:
+                      Timer.wb(addr, val);
+                      break;
+                    case 15:
+                      MMU._if = val;
+                      break;
+                  }
+                  break;
 
-              case 0x10:
-              case 0x20:
-              case 0x30:
-                break;
+                case 0x10:
+                case 0x20:
+                case 0x30:
+                  break;
 
-              case 0x40:
-              case 0x50:
-              case 0x60:
-              case 0x70:
-                GPU.wb(addr, val);
-                break;
+                case 0x40:
+                case 0x50:
+                case 0x60:
+                case 0x70:
+                  GPU.wb(addr, val);
+                  break;
+              }
             }
         }
         break;
     }
   },
 
-  ww: function(addr, val) {
+  ww(addr, val) {
     MMU.wb(addr, val & 0xFF);
     MMU.wb(addr + 1, val >> 8);
-  }
+  },
 };
 
 export default MMU;
